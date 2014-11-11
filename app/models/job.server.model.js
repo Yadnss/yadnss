5851d69d137b71b1516069eb683ab31d75398895
Alex Hartsell <kalhartt@gmail.com>
2014-11-10 22:48:04 -0500

    Add Job model/routes and server tests

---
+++ app/models/job.server.model.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Job Schema
+ */
+var JobSchema = new Schema({
+	name: {
+		type: String,
+		default: '',
+		required: 'Please fill Job name',
+		trim: true
+	},
+	parent: {
+		type: Schema.ObjectId,
+		ref: 'Job'
+	}
+});
+
+mongoose.model('Job', JobSchema);
---
+++ app/tests/job.server.model.test.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var should = require('should'),
+	mongoose = require('mongoose'),
+	Job = mongoose.model('Job');
+
+/**
+ * Globals
+ */
+var job, child;
+
+/**
+ * Unit tests
+ */
+describe('Job Model Unit Tests:', function() {
+	beforeEach(function(done) {
+
+		job = new Job({
+			name: 'Job'
+		});
+		child = new Job({
+			name: 'Child Job',
+			parent: job
+		});
+
+		done();
+	});
+
+	describe('Method Save', function() {
+		it('should be able to save without problems', function(done) {
+			return job.save(function(err) {
+				should.not.exist(err);
+				child.save(function(err) {
+					should.not.exist(err);
+					done();
+				});
+			});
+		});
+
+		it('should be able to show an error when try to save without name', function(done) { 
+			job.name = '';
+
+			return job.save(function(err) {
+				should.exist(err);
+				done();
+			});
+		});
+	});
+
+	afterEach(function(done) { 
+		Job.remove().exec();
+
+		done();
+	});
+});
