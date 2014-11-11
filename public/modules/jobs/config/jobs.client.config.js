1be1f9b4ccc8f4b4d7c3679ed06a45cdea497bcb
Alex Hartsell <kalhartt@gmail.com>
2014-11-11 04:24:02 -0500

    Hide Jobs admin pages from users

--- public/modules/jobs/config/jobs.client.config.js
+++ public/modules/jobs/config/jobs.client.config.js
@@ -4,7 +4,7 @@
 angular.module('jobs').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Jobs', 'jobs', 'dropdown', '/jobs(/create)?');
+		Menus.addMenuItem('topbar', 'Jobs', 'jobs', 'dropdown', '/jobs(/create)?', false, ['admin']);
 		Menus.addSubMenuItem('topbar', 'jobs', 'List Jobs', 'jobs');
 		Menus.addSubMenuItem('topbar', 'jobs', 'New Job', 'jobs/create');
 	}
