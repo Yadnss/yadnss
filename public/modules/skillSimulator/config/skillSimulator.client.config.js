5b53411b25a4dc6d202574a4c82ba5c1d5056d14
Alex Hartsell <kalhartt@gmail.com>
2014-11-18 02:20:42 -0500

    Add skillSimulator module with dynamicly generated menu

---
+++ public/modules/skillSimulator/config/skillSimulator.client.config.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// Configuring the skillSimulator module
+angular.module('skillSimulator').run(['Menus', 'Jobs',
+	function(Menus, Jobs) {
+		Jobs.query(function(jobs) {
+			var baseJobs, t1Jobs, t2Jobs;
+
+			// Find starting jobs
+			baseJobs = _.filter(jobs, function(job) { return job.tier === 0; });
+
+			_.forEach(baseJobs, function(baseJob) {
+				// Make a menu item for each starting job
+				Menus.addMenuItem('topbar', baseJob.name, baseJob.name, 'dropdown', '#');
+
+				// Find their first specialization classes
+				t1Jobs = _.filter(jobs, function(job) { return job.parent && job.parent._id === baseJob._id; });
+				_.forEach(t1Jobs, function(t1Job, index) {
+					// Add header menu items for first spec class
+					if (index) {
+						Menus.addSubMenuItem('topbar', baseJob.name, t1Job.name + 'divider', '#', null, null, null, null, 'divider');
+					}
+					Menus.addSubMenuItem('topbar', baseJob.name, t1Job.name, '#', null, null, null, null, 'dropdown-header');
+
+					t2Jobs = _.filter(jobs, function(job) { return job.parent && job.parent._id === t1Job._id; });
+					_.forEach(t2Jobs, function(t2Job) {
+						Menus.addSubMenuItem('topbar', baseJob.name, t2Job.name, 'simulator/' + t2Job._id);
+					});
+				});
+			});
+		});
+	}
+]);
